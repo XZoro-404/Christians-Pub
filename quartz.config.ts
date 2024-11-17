7f3cc978cf4325e96b34e8ac7ff9d51442960558
Christian <blac7616@kettering.edu>
2024-11-17 18:14:38 +0100

    Quartz sync: Nov 17, 2024, 6:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "xzoro-404.github.io/Christians-Pub",
+    baseUrl: "https://xzoro-404.github.io/Christians-Pub/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
